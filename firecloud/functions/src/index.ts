c21ee19c7df737087ea63696cbe3bbfcffdc0c05
Piyapat <piyapat.pai@outlook.com>
2025-01-07 21:50:56 +0700

    commented out unnecessary functions

--- firecloud/functions/src/index.ts
+++ firecloud/functions/src/index.ts
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onRequest } from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import { onRequest } from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
@@ -23,10 +23,10 @@ const db = getFirestore(app);
 
 export { db };
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
 
 import { registerUsers } from "./registerUsers";
 export { registerUsers };
