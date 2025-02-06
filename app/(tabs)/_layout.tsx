7c076883349206ff5295a25c5c218e9b76986312
Piyapat <piyapat.pai@outlook.com>
2025-02-07 01:06:25 +0700

    using pill style for footer

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,40 +2,67 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { useUserSession } from "../auth/firebaseAuth";
+import { View, StyleSheet } from "react-native";
 
 export default function TabsLayout() {
   const userInfoFromStore = useUserSession((state) => state.userInfo);
 
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="HomeScreen"
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: styles.tabBar,
+          tabBarItemStyle: styles.tabBarItem,
+          tabBarLabelStyle: styles.tabBarLabel,
         }}
-      />
-      <Tabs.Screen
-        name="ProfileScreen"
-        options={{
-          tabBarLabel: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="SubmitProductScreen"
-        options={{
-          tabBarLabel: "Submit Product",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="add-circle" size={size} color={color} />
-          ),
-          tabBarButton: userInfoFromStore.role === "seller" ? undefined : () => null,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="HomeScreen"
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="ProfileScreen"
+          options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="SubmitProductScreen"
+          options={{
+            tabBarLabel: "Submit",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="add-circle" size={size} color={color} />
+            ),
+            tabBarButton:
+              userInfoFromStore.role === "seller" ? undefined : () => null,
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
+    height: 60,
+    borderRadius: 25,
+    backgroundColor: "white",
+  },
+  tabBarItem: {
+    paddingVertical: 10,
+  },
+  tabBarLabel: { fontSize: 12, fontWeight: "700" },
+});
