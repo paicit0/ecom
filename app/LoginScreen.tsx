4cad5be74e377997b0318e52f33aeefe22bac056
Piyapat <piyapat.pai@outlook.com>
2025-02-06 15:05:10 +0700

    added loading state, redirects to homescreen on successful login

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -1,22 +1,60 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Link } from "expo-router";
+import { Link, Redirect, useRouter } from "expo-router";
 import { useState } from "react";
-import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import { useUserSession } from "./auth/firebaseAuth";
 function LoginScreen() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
   const login = useUserSession((state) => state.login);
+  const router = useRouter();
 
   const handleLogin = async () => {
+    setLoading(true);
     try {
-      console.log("Trying to login... ", email);
-      login(email, password);
+      console.log("LoginScreen: Trying to login... ", email);
+      const tryLogin = await login(email, password);
+      console.log("LoginScreen: tryLogin returns:", tryLogin);
+      if (tryLogin.success) {
+        console.log(
+          "LoginScreen: tryLogin.success redirecting to:../(tabs)/HomeScreen"
+        );
+        router.replace("../(tabs)/HomeScreen");
+        return;
+      } else {
+        console.error("LoginScreen: Login failed");
+      }
     } catch (error) {
-      console.log(error);
+      console.error("LoginScreen: Error during login:", error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignContent: "center",
+          justifyContent: "center",
+          alignSelf: "center",
+        }}
+      >
+        <Text>Trying to get you signed in...</Text>
+        <ActivityIndicator size="large" color="green" />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -37,7 +75,7 @@ function LoginScreen() {
         value={password}
         onChangeText={(text) => setPassword(text)}
       ></TextInput>
-      <Pressable onPress={() => handleLogin()}>
+      <Pressable onPress={handleLogin}>
         <Text>Login!</Text>
       </Pressable>
     </View>
