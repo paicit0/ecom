c79e3f72c0c8779c57a6397bd75e8afa2fcdaab6
Piyapat <piyapat.pai@outlook.com>
2025-02-11 13:53:52 +0700

    added apu image. will animate svg later

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -4,6 +4,13 @@ import { useState } from "react";
 import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
 import { useUserSession } from "./auth/firebaseAuth";
 import AnimatedLoadingIndicator from "../components/AnimatedLoadingIndicator";
+import { Image } from "expo-image";
+import { Svg, G } from "react-native-svg";
+import apuPNG from "../assets/images/apuPNG.png";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 function LoginScreen() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -39,6 +46,27 @@ function LoginScreen() {
     }
   };
 
+  const eyePosition = useSharedValue(0);
+  const textPosition = useSharedValue(0);
+  const animatedLeftEyeStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scaleY: eyePosition.value === 0 ? 1 : 0 }],
+    };
+  });
+
+  const animatedRightEyeStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scaleY: eyePosition.value === 0 ? 1 : 0 }],
+    };
+  });
+
+  const handleEyesPosition = () => {
+    return (eyePosition.value = eyePosition.value + 50);
+  };
+  const handleTextPosition = () => {
+    return (textPosition.value = textPosition.value + 1);
+  };
+
   if (loading) {
     return (
       <View style={{ marginTop: 60 }}>
@@ -49,6 +77,53 @@ function LoginScreen() {
 
   return (
     <View style={styles.container}>
+      <Image source={apuPNG} style={{ height: 200, width: 200 }} />
+      <Svg
+        width={200}
+        height={200}
+        viewBox="0 0 200 200"
+        style={{ position: "absolute", top: 0, left: 0 }}
+      >
+        <G>
+          <G>
+            <Animated.View
+              style={[
+                {
+                  position: "absolute",
+                  left: 190,
+                  top: 310,
+                  width: 32,
+                  height: 25,
+                  borderRadius: 15,
+                  backgroundColor: "black",
+                },
+                animatedLeftEyeStyle,
+              ]}
+            />
+          </G>
+
+          <G>
+            <Animated.View
+              style={[
+                {
+                  position: "absolute",
+                  left: 260,
+                  top: 315,
+                  width: 30,
+                  height: 25,
+                  borderRadius: 12,
+                  backgroundColor: "black",
+                },
+                animatedRightEyeStyle,
+              ]}
+            />
+          </G>
+        </G>
+      </Svg>
+      <Pressable onPress={handleEyesPosition}>
+        <Text>Big eye</Text>
+      </Pressable>
+
       <Link href="../(tabs)/HomeScreen">
         <Ionicons name="arrow-back-outline" size={20}></Ionicons>
       </Link>
