19c2d6ab9de123ba32561259dcd28c21720ab375
Piyapat <piyapat.pai@outlook.com>
2025-02-11 13:53:22 +0700

    moving wrappers to _layout instead since that's the main app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,41 @@
 // app/_layout.tsx
+import { StripeProvider } from "@stripe/stripe-react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
+  const [publishableKey, setPublishableKey] = useState("");
+
+  const fetchPublishableKey = async () => {
+    const key = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+    if (!key) {
+      console.log("app/_layout: No EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY");
+      return;
+    }
+    setPublishableKey(key);
+  };
+
+  useEffect(() => {
+    fetchPublishableKey();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <StripeProvider
+        publishableKey={publishableKey}
+        urlScheme="your-url-scheme"
+      >
+        <View style={styles.container}>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </View>
+      </StripeProvider>
+    </QueryClientProvider>
   );
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,8 @@
 //eas build --profile development --platform android
 //Index.tsx
 import { Redirect } from "expo-router";
-import { StripeProvider } from "@stripe/stripe-react-native";
-import { useState, useEffect } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 function Index() {
-  const [publishableKey, setPublishableKey] = useState("");
-
-  const fetchPublishableKey = async () => {
-    const key = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-    if (!key) {
-      console.log("No EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY");
-      return;
-    }
-    setPublishableKey(key);
-    console.log("Index: StripePublishableKey", publishableKey);
-  };
-
-  useEffect(() => {
-    fetchPublishableKey();
-  }, []);
-
   console.log(
     "Index: Launching app in mode:",
     process.env.EXPO_PUBLIC_CURRENT_APP_MODE
@@ -36,16 +15,7 @@ function Index() {
     console.debug = function no_console() {};
   }
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <StripeProvider
-        publishableKey={publishableKey}
-        urlScheme="your-url-scheme"
-      >
-        <Redirect href="../(tabs)/HomeScreen" />
-      </StripeProvider>
-    </QueryClientProvider>
-  );
+  return <Redirect href="../(tabs)/HomeScreen" />;
 }
 
 export default Index;
