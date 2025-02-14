d131d3a2acf39121dd8344b23d11685457c50388
Piyapat <piyapat.pai@outlook.com>
2025-02-14 16:16:16 +0700

    using query/mutation to get/delete favorite

--- app/FavoriteScreen.tsx
+++ app/FavoriteScreen.tsx
@@ -1,99 +1,67 @@
 import { useEffect, useState } from "react";
-import { Pressable, Text, StyleSheet, View, ScrollView } from "react-native";
-import { Product, useFavorite } from "./store/store";
+import {
+  Pressable,
+  Text,
+  StyleSheet,
+  View,
+  ScrollView,
+  Image,
+} from "react-native";
+import { Product } from "./store/store";
 import { Ionicons } from "@expo/vector-icons";
-import axios from "axios";
 import { getAuth } from "firebase/auth";
-import * as SecureStore from "expo-secure-store";
 import { Link } from "expo-router";
 import AnimatedLoadingIndicator from "../components/AnimatedLoadingIndicator";
 import { FlashList } from "@shopify/flash-list";
 import { useGetFavorite } from "../hooks/fetch/useGetFavorite";
-import { useUpdateFavorite } from "../hooks/fetch/useUpdateFavorite";
+import { useDeleteFavorite } from "../hooks/fetch/useDeleteFavorite";
 
 function FavoriteScreen() {
   const [loading, setLoading] = useState<boolean>(false);
-
-  const favoriteItemsArray = useFavorite((state) => state.favoriteItemsArray);
-  const deleteFromFavorite = useFavorite((state) => state.deleteFromFavorite);
-  const deleteAllFavorite = useFavorite((state) => state.deleteAllFavorite);
-
   const auth = getAuth();
   const userAuth = auth.currentUser;
-
-  const {
-    data: favorites,
-    isPending,
-    isError,
-    error,
-    refetch,
-  } = useGetFavorite(userAuth?.email as string);
-
-  const handleUpdateFavorite = async () => {
-    if (!userAuth) {
-      console.log("FavoriteScreen: no userAuth");
-      return;
-    }
-    console.log("FavoriteScreen: userAuth.uid: ", userAuth.uid);
-
-    setLoading(true);
-    try {
-      if (!userAuth.email || !favoriteItemsArray.length) {
-        return;
-      }
-
-      const idToken = await SecureStore.getItemAsync("authToken");
-      console.log("idToken:", idToken);
-      const updateFavoriteUrl =
-        process.env.EXPO_PUBLIC_CURRENT_APP_MODE === "dev"
-          ? process.env.EXPO_PUBLIC_updateFavorite_emulator
-          : process.env.EXPO_PUBLIC_updateFavorite_prod;
-      if (!updateFavoriteUrl) {
-        console.error("FavoriteScreen:not bussin urls");
-        return;
-      }
-      console.log("FavoriteScreen: updateFavoriteUrl:", updateFavoriteUrl);
-      console.log(
-        `FavoriteScreen: Payload to updateFavorite:Email:${userAuth.email} Array:`,
-        favoriteItemsArray
-      );
-      const updateFavoriteFromItemsArray = await axios.post(
-        updateFavoriteUrl,
-        { email: userAuth.email, favoriteItemsArray: favoriteItemsArray },
-        {
-          headers: {
-            Authorization: `Bearer ${idToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(
-        "FavoriteScreen: update.status",
-        updateFavoriteFromItemsArray.status
-      );
-      if (updateFavoriteFromItemsArray.status === 201) {
-        return true;
-      }
-    } catch (error) {
-      console.error("FavoriteScreen: updateFavorite internal errors:", error);
-      return false;
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    refetch();
-
-    console.log("HEY", favorites);
-    // handleUpdateFavorite();
-  }, [favoriteItemsArray]);
-
-  const render = () => {
-    return <></>
+  if (!userAuth) {
+    console.error("No user auth found");
+    return;
+  }
+  const userEmail = userAuth.email;
+  if (!userEmail || !userAuth) {
+    console.error("No user email found");
+    return;
   }
 
-  if (!favorites) {
+  const getFavoriteQuery = useGetFavorite({ userEmail });
+  const deleteFavoriteMutation = useDeleteFavorite();
+
+  useEffect(() => {
+    try {
+      getFavoriteQuery.refetch();
+      console.log(
+        "FavoriteScreen: getFavoriteQuery.data:",
+        getFavoriteQuery.data
+      );
+    } catch (error) {
+      console.error("FavoriteScreen: ", error);
+    }
+  }, []);
+
+  const render = () => {
+    return <></>;
+  };
+
+  if (getFavoriteQuery.isError) {
+    return (
+      <View>
+        <Text>Error getting favorite.</Text>
+      </View>
+    );
+  }
+
+  if (!getFavoriteQuery.data) {
+    return <></>;
+  }
+
+  if (getFavoriteQuery.data.favoriteProducts.length === 0) {
     return (
       <View
         style={{
@@ -107,7 +75,8 @@ function FavoriteScreen() {
       </View>
     );
   }
-  if (isPending) {
+
+  if (getFavoriteQuery.isLoading) {
     setTimeout(() => {
       return (
         <View style={{ marginTop: 60 }}>
@@ -129,19 +98,43 @@ function FavoriteScreen() {
         </View>
 
         <View style={styles.favoriteItemsArrayContainer}>
-          {favorites.favoriteProducts.map((product: Product, index: number) => (
-            <View key={index} style={{}}>
-              <Text>{product.productName}</Text>
-              <Text>{product.productDescription}</Text>
-              <Text>{product.productImageUrl[0]}</Text>
-              <Pressable onPress={() => deleteFromFavorite(product.toString())}>
-                <Ionicons name="close-sharp" size={20} color="#666" />
-              </Pressable>
-            </View>
-          ))}
+          {getFavoriteQuery.data.favoriteProducts.map(
+            (product: Product, index: number) => (
+              <View key={index} style={{}}>
+                <Link
+                  href={{
+                    pathname: "/ItemScreen/[id]",
+                    params: { id: product.productId },
+                  }}
+                  asChild
+                >
+                  <Pressable>
+                    <Image
+                      style={{ height: 150, width: 150 }}
+                      source={{ uri: product.productThumbnailUrl[0] }}
+                    />
+                    <Text>{product.productName}</Text>
+                    <Text>{product.productDescription}</Text>
+                    <Text>{product.productThumbnailUrl[0]}</Text>
+                  </Pressable>
+                </Link>
+
+                <Pressable
+                  onPress={() => {
+                    deleteFavoriteMutation.mutate({
+                      userEmail: userEmail,
+                      productId: product.productId,
+                    });
+                  }}
+                >
+                  <Ionicons name="close-sharp" size={20} color="#666" />
+                </Pressable>
+              </View>
+            )
+          )}
         </View>
-        {favoriteItemsArray.length > 0 && (
-          <Pressable onPress={deleteAllFavorite}>
+        {getFavoriteQuery.data.favoriteProducts.length > 0 && (
+          <Pressable onPress={() => console.log("delete all placeholder")}>
             <Text>Delete All</Text>
           </Pressable>
         )}
