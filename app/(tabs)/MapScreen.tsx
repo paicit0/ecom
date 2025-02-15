e662557da8c014257d22c543b4b6cfe33ddd1718
Piyapat <piyapat.pai@outlook.com>
2025-02-15 17:03:54 +0700

    mapscreen boilerplate

---
+++ app/(tabs)/MapScreen.tsx
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+import MapView from "react-native-maps";
+import { PROVIDER_GOOGLE } from "react-native-maps";
+
+function MapScreen() {
+  return (
+    <View style={{}}>
+      <MapView provider={PROVIDER_GOOGLE} style={{}} />
+    </View>
+  );
+}
+
+export default MapScreen;
