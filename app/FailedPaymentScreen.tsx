c29cbd4124cba2f2ef43192c1fa87263fe0887b1
Piyapat <piyapat.pai@outlook.com>
2025-03-06 16:46:22 +0700

    ui fixes

--- app/FailedPaymentScreen.tsx
+++ app/FailedPaymentScreen.tsx
@@ -5,13 +5,31 @@ import { Pressable, View, Text } from "react-native";
 function FailedPaymentScreen() {
   const router = useRouter();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignContent: "center" }}>
-      <Ionicons name="close-circle-outline" size={40}></Ionicons>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Ionicons
+        name="close-circle-outline"
+        size={40}
+        style={{ color: "red" }}
+      ></Ionicons>
       <View>
         <Text>The payment was not successful!</Text>
       </View>
-      <Pressable onPress={() => router.replace("/(tabs)/HomeScreen")}>
-        <Text>Go back?</Text>
+      <Pressable
+        style={{
+          backgroundColor: "#fff",
+          padding: 10,
+          borderRadius: 5,
+          marginTop: 10,
+        }}
+        onPress={() => router.replace("/(tabs)/HomeScreen")}
+      >
+        <Text>Go back</Text>
       </Pressable>
     </View>
   );
--- app/SucceededPaymentScreen.tsx
+++ app/SucceededPaymentScreen.tsx
@@ -5,13 +5,27 @@ import { Pressable, View, Text } from "react-native";
 function SucceededPaymentScreen() {
   const router = useRouter();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignContent: "center" }}>
-      <Ionicons name="checkmark-circle-outline" size={40}></Ionicons>
+    <View style={{         flex: 1,
+      justifyContent: "center",
+      alignItems: "center",}}>
+      <Ionicons
+        name="checkmark-circle-outline"
+        size={40}
+        style={{ color: "green" }}
+      ></Ionicons>
       <View>
-        $500 Paid!<Text></Text>
+        <Text>$500 Paid!</Text>
       </View>
-      <Pressable onPress={() => router.replace("/(tabs)/HomeScreen")}>
-        <Text>Go back?</Text>
+      <Pressable
+        style={{
+          backgroundColor: "#fff",
+          padding: 10,
+          borderRadius: 5,
+          marginTop: 10,
+        }}
+        onPress={() => router.replace("/(tabs)/HomeScreen")}
+      >
+        <Text>Go back</Text>
       </Pressable>
     </View>
   );
