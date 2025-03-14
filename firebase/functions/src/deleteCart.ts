7faa61b700674d0b5660798fc073e4dbb01b9aa1
Piyapat <piyapat.pai@outlook.com>
2025-03-14 16:01:22 +0700

    feat: implement transaction handling in deleteCart function to manage cart item removal

--- firebase/functions/src/deleteCart.ts
+++ firebase/functions/src/deleteCart.ts
@@ -1,6 +1,5 @@
 // deleteCart.ts
 import * as functions from "firebase-functions";
-import { FieldValue } from "firebase-admin/firestore";
 import { db } from "./index";
 import express from "express";
 import verifyBearerAndIdtoken from "./middlewares/verifyBearerAndIdtoken";
@@ -12,7 +11,7 @@ app.use(verifyBearerAndIdtoken);
 app.post("/", async (req, res) => {
   const { userEmail, productId } = req.body;
 
-  console.log("deleteCart: req.body: ", userEmail, productId);
+  console.log("deleteCart: req.body:", userEmail, productId);
 
   if (!userEmail || !productId) {
     return res.status(401).json({
@@ -21,37 +20,62 @@ app.post("/", async (req, res) => {
   }
 
   try {
-    const usersSnapshot = await db
-      .collection("users")
-      .where("userEmail", "==", userEmail)
-      .get();
+    await db.runTransaction(async (transaction) => {
+      const usersSnapshot = await db
+        .collection("users")
+        .where("userEmail", "==", userEmail)
+        .get();
+      console.log("deleteCart: usersSnapshot.size:", usersSnapshot.size);
 
-    console.log("deleteCart: usersSnapshot:", usersSnapshot);
+      if (usersSnapshot.empty) {
+        console.error("deleteCart: No user found with userEmail:", userEmail);
+        return res.status(404).json({ error: "deleteCart: User not found." });
+      }
 
-    if (usersSnapshot.empty) {
-      console.error("deleteCart: No user found with userEmail:", userEmail);
-      return res.status(404).json({ error: "deleteCart: User not found." });
-    }
+      const userDoc = usersSnapshot.docs[0];
+      console.log("deleteCart: userDoc.id:", userDoc.id);
 
-    const userDoc = usersSnapshot.docs[0];
-    console.log("deleteCart: userDoc.id:", userDoc.id);
-    if (!userDoc.exists) {
-      console.error(
-        "deleteCart: No user data found with userEmail:",
-        userEmail
+      const userData = userDoc.data() || {};
+
+      const cartItemsArray: { productId: string; productQuantity: number }[] =
+        userData.cartItemsArray;
+
+      console.log("deleteCart: cartItemsArray:", cartItemsArray);
+
+      const updatedCartItems = [...cartItemsArray];
+
+      const existingItemIndex = updatedCartItems.findIndex(
+        (item) => item.productId === productId
       );
-      return res
-        .status(404)
-        .json({ error: "deleteCart: User data not found." });
-    }
 
-    console.log("deleteCart: userDoc updating.");
-    await userDoc.ref.update({
-      cartItemsArray: FieldValue.arrayRemove(productId),
+      if (existingItemIndex !== -1) {
+        console.log("deleteCart: Decreasing existing product quantity by 1");
+        updatedCartItems[existingItemIndex] = {
+          ...updatedCartItems[existingItemIndex],
+          productQuantity:
+            updatedCartItems[existingItemIndex].productQuantity - 1,
+        };
+
+        if (updatedCartItems[existingItemIndex].productQuantity <= 0) {
+          console.log("deleteCart: Removing the product from cart");
+          updatedCartItems.splice(existingItemIndex, 1);
+        }
+      } else {
+        console.log(
+          "deleteCart: the product you are trying to delete doesn't exist!"
+        );
+        return res.status(404).json({
+          message:
+            "deleteCart: the product you are trying to delete doesn't exist!",
+        });
+      }
+
+      transaction.update(userDoc.ref, { cartItemsArray: updatedCartItems });
+      return true;
     });
-    return res
-      .status(200)
-      .json({ message: "deleteCart: Item removed from cartItemsArray" });
+    return res.status(200).json({
+      message: "deleteCart: execution finished",
+    });
   } catch (error) {
     console.error("deleteCart: internal error:", error);
     return res.status(401).json({ error: "deleteCart: internal errors" });
