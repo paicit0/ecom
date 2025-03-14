7b8b22a1716019002db8b0e663b0d55eec2a273d
Piyapat <piyapat.pai@outlook.com>
2025-03-14 23:49:48 +0700

    chore: update dependencies and configuration for improved performance and compatibility

--- app.config.ts
+++ app.config.ts
@@ -36,7 +36,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     output: "static",
     favicon: "./assets/images/favicon.png",
   },
-  plugins: ["expo-router", "expo-secure-store"],
+  plugins: ["expo-router", "expo-secure-store", "expo-font"],
   experiments: {
     typedRoutes: true,
   },
