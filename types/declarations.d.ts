88e8116b8dd84484430ba877315f33d11ff1aff7
Piyapat <piyapat.pai@outlook.com>
2025-03-24 15:08:23 +0700

    refactor: move these files outside the app folder

---
+++ types/declarations.d.ts
@@ -0,0 +1,11 @@
+declare module '*.png' {
+    import { ImageSourceProp } from 'react-native';
+    const value: ImageSourceProp;
+    export default value;
+  }
+  
+  declare module '*.jpg';
+  declare module '*.jpeg'; 
+  declare module '*.gif';
+  declare module '*.webp'; 
+  declare module '*.svg'; 
