a28f50540314732a92df6db209ac75e31d15994b
Piyapat <piyapat.pai@outlook.com>
2025-03-25 11:36:23 +0700

    refactor: loggings

--- firebase/functions/src/middlewares/verifyBearerAndIdtoken.ts
+++ firebase/functions/src/middlewares/verifyBearerAndIdtoken.ts
@@ -7,12 +7,17 @@ const verifyBearerAndIdtoken = async function (
   res: Response,
   next: NextFunction
 ) {
-  console.log("verifyBearerAndIdtoken: req.header: ", req.headers.authorization);
+  console.log(
+    "verifyBearerAndIdtoken: req.header: ",
+    req.headers.authorization
+  );
   console.log("verifyBearerAndIdtoken: req.body: ", req.body);
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.toLowerCase().startsWith("bearer")) {
     console.log("verifyBearerAndIdtoken: no/invalid auth in headers!");
-    res.status(401).json({ error: "verifyBearerAndIdtoken: no auth in headers!" });
+    res
+      .status(401)
+      .json({ error: "verifyBearerAndIdtoken: no auth in headers!" });
     return;
   }
   const idToken = authHeader.replace(/^Bearer\s+/i, "").trim();
@@ -25,7 +30,9 @@ const verifyBearerAndIdtoken = async function (
     }
   } catch (error) {
     console.log(error);
-    res.status(401).json({ error: `verifyBearerAndIdtoken: Unauthorized! ${error}` });
+    res
+      .status(401)
+      .json({ error: `verifyBearerAndIdtoken: Unauthorized! ${error}` });
     return;
   }
   next();
