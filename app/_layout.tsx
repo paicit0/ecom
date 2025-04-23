dd566c9165b674651c7c1b2f4f78c450f0ab740f
Piyapat <piyapat.pai@outlook.com>
2025-04-23 14:14:25 +0700

    fix: standardize formatting and indentation in _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,63 +1,63 @@
-// app/_layout.tsx
-import { StripeProvider } from "@stripe/stripe-react-native";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
-import { useState, useEffect } from "react";
-import { View, StyleSheet } from "react-native";
+  // app/_layout.tsx
+  import { StripeProvider } from "@stripe/stripe-react-native";
+  import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+  import { Stack } from "expo-router";
+  import { useState, useEffect } from "react";
+  import { View, StyleSheet } from "react-native";
 
-export default function RootLayout() {
-  const [publishableKey, setPublishableKey] = useState("");
-  const queryClient = new QueryClient();
+  export default function RootLayout() {
+    const [publishableKey, setPublishableKey] = useState("");
+    const queryClient = new QueryClient();
 
-  const fetchPublishableKey = async () => {
-    const key = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-    if (!key) {
-      console.log("app/_layout: No EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY");
-      return;
-    }
-    setPublishableKey(key);
-  };
+    const fetchPublishableKey = async () => {
+      const key = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+      if (!key) {
+        console.log("app/_layout: No EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY");
+        return;
+      }
+      setPublishableKey(key);
+    };
 
-  useEffect(() => {
-    fetchPublishableKey();
-  }, []);
+    useEffect(() => {
+      fetchPublishableKey();
+    }, []);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <StripeProvider
-        publishableKey={publishableKey}
-        urlScheme="your-url-scheme"
-      >
-        <View style={styles.container}>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-            }}
-          >
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="ItemScreen/[id]"
-              options={{
-                gestureEnabled: true,
-                gestureDirection: "horizontal",
-                animation: "slide_from_right",
+    return (
+      <QueryClientProvider client={queryClient}>
+        <StripeProvider
+          publishableKey={publishableKey}
+          urlScheme="your-url-scheme"
+        >
+          <View style={styles.container}>
+            <Stack
+              screenOptions={{
+                headerShown: false,
               }}
-            />
-          </Stack>
-        </View>
-      </StripeProvider>
-    </QueryClientProvider>
-  );
-}
+            >
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="ItemScreen/[id]"
+                options={{
+                  gestureEnabled: true,
+                  gestureDirection: "horizontal",
+                  animation: "slide_from_right",
+                }}
+              />
+            </Stack>
+          </View>
+        </StripeProvider>
+      </QueryClientProvider>
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  title: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold",
-    paddingBottom: 10,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    title: {
+      color: "#fff",
+      fontSize: 18,
+      fontWeight: "bold",
+      paddingBottom: 10,
+    },
+  });
