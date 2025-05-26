da8653fee772eda6c2795cde330ff7b6af15f4b4
Piyapat <piyapat.pai@outlook.com>
2025-05-26 12:29:40 +0700

    refactor: renamed useAddFavorite to useCreateFavorite

--- hooks/fetch/useCreateFavorite.tsx
+++ hooks/fetch/useCreateFavorite.tsx
@@ -1,42 +1,42 @@
-// useAddFavorite.tsx
+// useCreateFavorite.tsx
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-type fetchAddFavoriteType = {
+type fetchCreateFavoriteType = {
   userEmail: string;
   productId: string;
 };
 
-type fetchAddFavoriteResponseSuccess = {
+type fetchCreateFavoriteResponseSuccess = {
   message: string;
 };
-type fetchAddFavoriteResponseFailed = {
+type fetchCreateFavoriteResponseFailed = {
   error: string;
 };
 
-type fetchAddFavoriteResponseType =
-  | fetchAddFavoriteResponseSuccess
-  | fetchAddFavoriteResponseFailed;
+type fetchCreateFavoriteResponseType =
+  | fetchCreateFavoriteResponseSuccess
+  | fetchCreateFavoriteResponseFailed;
 
-const fetchAddFavorite = async ({
+const fetchCreateFavorite = async ({
   userEmail,
   productId,
-}: fetchAddFavoriteType): Promise<fetchAddFavoriteResponseType> => {
+}: fetchCreateFavoriteType): Promise<fetchCreateFavoriteResponseType> => {
   try {
     if (!userEmail) throw new Error("User userEmail is required");
 
     const idToken = await SecureStore.getItemAsync("authToken");
-    const addFavoriteUrl =
+    const createFavoriteUrl =
       process.env.EXPO_PUBLIC_CURRENT_APP_MODE === "dev"
-        ? process.env.EXPO_PUBLIC_addFavorite_emulator
-        : process.env.EXPO_PUBLIC_addFavorite_prod;
+        ? process.env.EXPO_PUBLIC_createFavorite_emulator
+        : process.env.EXPO_PUBLIC_createFavorite_prod;
 
-    if (!addFavoriteUrl) throw new Error("addFavoriteUrl not found");
-    console.log("useAddFavorite: payload:", userEmail, productId);
+    if (!createFavoriteUrl) throw new Error("createFavoriteUrl not found");
+    console.log("useCreateFavorite: payload:", userEmail, productId);
 
     const { data } = await axios.post(
-      addFavoriteUrl,
+      createFavoriteUrl,
       { userEmail, productId },
       {
         headers: {
@@ -47,16 +47,16 @@ const fetchAddFavorite = async ({
     );
     return data;
   } catch (error) {
-    console.error("useAddFavorite: ", error);
+    console.error("useCreateFavorite: ", error);
     throw error;
   }
 };
 
-export const useAddFavorite = () => {
+export const useCreateFavorite = () => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({ userEmail, productId }: fetchAddFavoriteType) =>
-      fetchAddFavorite({ userEmail, productId }),
+    mutationFn: ({ userEmail, productId }: fetchCreateFavoriteType) =>
+      fetchCreateFavorite({ userEmail, productId }),
     onSettled: async () => {
       return await queryClient.invalidateQueries({ queryKey: ["favorites"] });
     },
