4b2a6a6ea3c1d542586d3ed9c6567df27498e473
Piyapat <piyapat.pai@outlook.com>
2025-05-26 12:02:09 +0700

    fix: react-native-svg-transformer/expo is likely not working with the new expo 53

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,10 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// Learn more https://docs.expo.io/guides/customizing-metro
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config;
+config.resolver.unstable_enablePackageExports = false;
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
-  };
-
-  return config;
-})();
+module.exports = config;
